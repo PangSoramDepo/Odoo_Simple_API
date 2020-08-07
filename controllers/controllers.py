66bb93249d601980342bfe78a06805a3eb8b7049
unknown <pangsoramdepo@gmail.com>
2020-08-07 16:58:10 +0700

    Return Value with Key

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -7,13 +7,21 @@ from .utils import Utils
 
 _logger = logging.getLogger(__name__)
 _AULR='/api/'
+_Log="--------------------------API---------------------------"
 
 class MyViewModule(http.Controller):
     @http.route(_AULR+'check-health', auth='public')
     def index(self, **kw):
         return "Health Is Still Alive"
 
-    @http.route(_AULR+'get-all-respartner',type='json',methods=["GET"], auth='public')
-    def res_partner_env(self, **kw):
+    @http.route(_AULR+'res-partner/all',type='json',methods=["GET"], auth='public')
+    def res_partner_all(self):
         partner=Utils('res.partner')
-        return partner.all()
+        data=partner.all()
+        del partner
+        return data
+
+    @http.route(_AULR+'res-partner/limit/<int:limit>',type='json',methods=["GET"], auth='public')
+    def res_partner_limit(self,limit):
+        partner=Utils('res.partner')
+        return partner.where('name','ilike','man').where('id','=',40).take(limit).get()
--- controllers/utils.py
+++ controllers/utils.py
@@ -3,29 +3,55 @@
 # Github : https://github.com/PangSoramDepo
 ###########################################
 
-from odoo.http import request
+import json
+
 from operator import attrgetter
+from odoo.http import request
+from odoo import http
 
 models={
-    'res.partner'   :   ['id','name','company_id','display_name']
+    'res.partner'   :   ['id','name','display_name']
 }
+
+g_take=5
+g_condition=[]
 
 class Utils:
     def __init__(self, model):
         self.model=model
+        self._restore()
 
     def all(self):
         return self._private_all()
 
-    def take(self,take):
-        return self._private_all(take)
+    def where(self,field,confition,value):
+        global g_condition
+        g_condition.append((field,confition,value))
+        return self
+
+    def take(self,take=5):
+        global g_take
+        g_take=take
+        return self
+
+    def get(self):
+        return self._private_all(g_take)
 
     def _private_all(self,limit=None):
-        if limit is None:
-            raw_data=request.env[self.model].search([])
-        else:
-            raw_data=request.env[self.model].search([],limit=limit)
+        raw_data=request.env[self.model].search(g_condition) if limit is None else request.env[self.model].search(g_condition,limit=limit)
         data=[]
         for x in raw_data:
-            data.append(attrgetter(*models[self.model])(x))
+            data.append(self._get_key_value(x,*models[self.model]))
         return data
+
+    def _get_key_value(self,obj,*arg):
+        data={}
+        for x in arg:
+            data[x]=attrgetter(x)(obj)
+        return data
+
+    def _restore(self):
+        global g_take
+        global g_condition
+        g_take=5
+        g_condition=[]
